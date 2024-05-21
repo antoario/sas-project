997c27ff510a082b7deb37ba7f3cf31e4c5ed1b7
edoardo <edoardobalzano01@gmail.com>
2024-05-21 10:23:26 +0200

    ok

--- 7 Codice/catering_init_db.sql
+++ 7 Codice/catering_init_db.sql
@@ -2,7 +2,7 @@
 --
 -- Host: 127.0.0.1    Database: catering
 -- ------------------------------------------------------
--- Server version	5.7.26
+-- Server version 5.7.26
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,10 +20,10 @@
 --
 
 CREATE TABLE `Roles` (
-  `id` char(1) DEFAULT NULL,
-  `role` varchar(128) DEFAULT NULL
+  `id` char(1) NOT NULL,
+  `role` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `Roles`
@@ -36,44 +36,14 @@ INSERT INTO `Roles` VALUES ('c','Cuoco'),('h','Chef'),('o','Organizzatore'),('s'
 UNLOCK TABLES;
 
 --
--- Table structure for table `UserRoles`
---
-
-CREATE TABLE `UserRoles` (
-  `user_id` int(11) DEFAULT NULL,
-  `role_id` char(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `UserRoles`
---
-
-LOCK TABLES `UserRoles` WRITE;
-/*!40000 ALTER TABLE `UserRoles` DISABLE KEYS */;
-INSERT INTO `UserRoles` VALUES 
-(1,'o'),
-(2,'h'),(2,'o'),
-(3,'h'),
-(4,'c'),(4,'h'),
-(5,'c'),
-(6,'c'),
-(7,'c'),(7,'s'),(7,'s'),
-(8,'s'),
-(9,'s'),
-(10,'s');
-/*!40000 ALTER TABLE `UserRoles` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `Users`
 --
 
 CREATE TABLE `Users` (
-  `id` int(11) DEFAULT NULL,
-  `username` varchar(128) DEFAULT NULL
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `Users`
@@ -94,6 +64,123 @@ INSERT INTO `Users` VALUES
 (10,'Piergiorgio');
 /*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `UserRoles`
+--
+
+CREATE TABLE `UserRoles` (
+  `user_id` int(11) NOT NULL,
+  `role_id` char(1) NOT NULL,
+  PRIMARY KEY (`user_id`, `role_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`),
+  FOREIGN KEY (`role_id`) REFERENCES `Roles`(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `UserRoles`
+--
+
+LOCK TABLES `UserRoles` WRITE;
+/*!40000 ALTER TABLE `UserRoles` DISABLE KEYS */;
+INSERT INTO `UserRoles` VALUES 
+(1,'o'),
+(2,'h'),(2,'o'),
+(3,'h'),
+(4,'c'),(4,'h'),
+(5,'c'),
+(6,'c'),
+(7,'c'),(7,'s'),
+(8,'s'),
+(9,'s'),
+(10,'s');
+/*!40000 ALTER TABLE `UserRoles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Turns`
+--
+
+CREATE TABLE `Turns` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(50) NOT NULL, -- 'Service' or 'Cooking'
+  `duration` int(11) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `deadline` datetime NOT NULL,
+  `date` datetime NOT NULL,
+  `is_recurring` boolean DEFAULT FALSE,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Turns`
+--
+
+LOCK TABLES `Turns` WRITE;
+/*!40000 ALTER TABLE `Turns` DISABLE KEYS */;
+INSERT INTO `Turns` (`type`, `duration`, `location`, `deadline`, `date`, `is_recurring`) VALUES
+('Cooking', 180, 'Kitchen A', '2024-05-30 12:00:00', '2024-06-01 08:00:00', FALSE),
+('Service', 240, 'Hall B', '2024-05-30 18:00:00', '2024-06-01 12:00:00', TRUE);
+/*!40000 ALTER TABLE `Turns` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Tasks`
+--
+
+CREATE TABLE `Tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) NOT NULL,
+  `assigned_to` int(11) DEFAULT NULL,
+  `role_required` char(1) DEFAULT NULL,
+  `turn_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `portions` int(11) DEFAULT NULL,
+  `due_time` datetime DEFAULT NULL,
+  `status` varchar(50) DEFAULT 'Pending',
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`assigned_to`) REFERENCES `Users`(`id`),
+  FOREIGN KEY (`role_required`) REFERENCES `Roles`(`id`),
+  FOREIGN KEY (`turn_id`) REFERENCES `Turns`(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `TaskAssignments`
+--
+
+CREATE TABLE `TaskAssignments` (
+  `task_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `assigned_date` datetime NOT NULL,
+  PRIMARY KEY (`task_id`, `user_id`),
+  FOREIGN KEY (`task_id`) REFERENCES `Tasks`(`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Tasks`
+--
+
+LOCK TABLES `Tasks` WRITE;
+/*!40000 ALTER TABLE `Tasks` DISABLE KEYS */;
+INSERT INTO `Tasks` (`description`, `assigned_to`, `role_required`, `turn_id`, `quantity`, `portions`, `due_time`, `status`) VALUES
+('Prepare appetizers', 4, 'c', 1, 50, 10, '2024-06-01 07:00:00', 'Pending'),
+('Serve main course', 8, 's', 2, 100, 20, '2024-06-01 13:00:00', 'Pending');
+/*!40000 ALTER TABLE `Tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `TaskAssignments`
+--
+
+LOCK TABLES `TaskAssignments` WRITE;
+/*!40000 ALTER TABLE `TaskAssignments` DISABLE KEYS */;
+INSERT INTO `TaskAssignments` (`task_id`, `user_id`, `assigned_date`) VALUES
+(1, 4, '2024-05-20 10:00:00'),
+(2, 8, '2024-05-21 11:00:00');
+/*!40000 ALTER TABLE `TaskAssignments` ENABLE KEYS */;
+UNLOCK TABLES;
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -103,4 +190,3 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
